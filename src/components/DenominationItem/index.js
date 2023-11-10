7f35a4c6fd63b0b1a28f2c6240684f1371cde478
Madhu_mitha <madhukiruba99@gmail.com>
2023-11-10 07:58:50 +0000

    Cash Withdrawl

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationAmountDetails, deductAmount} = props
+  const {value} = denominationAmountDetails
+
+  const oneClickDeductAmount = () => {
+    deductAmount(value)
+  }
+  return (
+    <li className="amount-list">
+      <button
+        type="button"
+        className="deduction-amount"
+        onClick={oneClickDeductAmount}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+export default DenominationItem
